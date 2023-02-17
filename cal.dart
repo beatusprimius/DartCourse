8dc36b75cc9dfc85d099fc0990a0d03f4433216e
Beatus Kamugisha <beatusprimius@gmail.com>
2023-02-17 12:25:38 +0300

    first commit

---
+++ cal.dart
@@ -0,0 +1,29 @@
+void main() {
+  print('Welcome to Calculator!');
+ 
+  // Assigning values to variables. 
+  int num1 = 4; 
+  int num2 = 6;
+  
+  // Adding two numbers 
+  int sum = num1 + num2; 
+ 
+  // Subtracting two numbers 
+  int difference = num1 - num2; 
+  
+  // Multiplying two numbers 
+  int product = num1 * num2; 
+  
+  // Dividing two numbers 
+  int quotient = num1 ~/ num2; 
+  
+  // Calculating the remainder 
+  int remainder = num1 % num2;
+ 
+  // Print all results 
+  print("Sum = $sum"); 
+  print("Difference = $difference"); 
+  print("Product = $product"); 
+  print("Quotient = $quotient"); 
+  print("Remainder = $remainder");
+}
